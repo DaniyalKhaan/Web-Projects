0661266994d3345d92167d0dbe98d02b604aa14d
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-02-09 07:13:20 +0500

    added functionality of mouse chaser

--- my_First_Website_Project/script.js
+++ my_First_Website_Project/script.js
@@ -2,3 +2,12 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function MouseChaserCircle(){
+    window.addEventListener("mousemove", function(dets) {
+        document.querySelector("#mouseChaserCircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+        
+
+    });
+}
+MouseChaserCircle();
