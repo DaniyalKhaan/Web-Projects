01c86c350020988e1eca757017ed648f072a5964
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-03-08 23:44:22 +0500

    working to get mouse cursor compressed when moving

--- my_First_Website_Project/script.js
+++ my_First_Website_Project/script.js
@@ -2,12 +2,32 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+let xprev = 0;
+let yprev = 0;
 
-function MouseChaserCircle(){
-    window.addEventListener("mousemove", function(dets) {
-        document.querySelector("#mouseChaserCircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+let xScale = 1;
+let yScale = 1;
+
+
+function circleCompressor(){
+    window.addEventListener("mouseove", function(dets){
+
+
+        xScale = gsap.util.clamp(.8,1.2, dets.clientX - xprev)
+        yScale = gsap.util.clamp(.8,1.2, dets.clientY - yprev)
+        xprev = dets.clientX;
+        yprev = dets.clientY;
+        mouseCursorChaser(xScale, yScale)
         
+    })
 
-    });
 }
-MouseChaserCircle();
+
+
+function mouseCursorChaser(xScale, yScale){
+    window.addEventListener("mousemove", function(getDetails){
+        this.document.getElementById("mouseChaserCircle").style.transform = `translate(${getDetails.clientX}px,${getDetails.clientY}px) scale(${xScale},${yScale})`;
+    })
+}
+// circleCompressor();
+mouseCursorChaser();
