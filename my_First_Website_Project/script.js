b15a66c96823b9b00f3d7e38f5bb7e0ff6f4fedb
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-03-11 03:35:07 +0500

    mouse chaser Circle skewing/compressing when moving is done.

--- my_First_Website_Project/script.js
+++ my_First_Website_Project/script.js
@@ -1,33 +1,39 @@
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+// Start - global variables used in mouseChaserCircle
 let xprev = 0;
 let yprev = 0;
-
 let xScale = 1;
 let yScale = 1;
+let cirleSizeToDefault = 0;
+let timeout;
+//  End - global variables used in mouseChaserCircle
 
+function mouseChaserCircle() {
+    clearTimeout(timeout);  // when mouse is moving value of timeout will be zero.
+    window.addEventListener("mousemove", function(dets){
+        xScale = gsap.utils.clamp(.8, 1.2 , dets.clientX - xprev);
+        yScale = gsap.utils.clamp(.7, 1.2 , dets.clientY - yprev);
 
-function circleCompressor(){
-    window.addEventListener("mouseove", function(dets){
-
-
-        xScale = gsap.util.clamp(.8,1.2, dets.clientX - xprev)
-        yScale = gsap.util.clamp(.8,1.2, dets.clientY - yprev)
         xprev = dets.clientX;
         yprev = dets.clientY;
-        mouseCursorChaser(xScale, yScale)
+
+        document.getElementById("mouseChaserCircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xScale}, ${yScale})`;
         
-    })
+        // timout will run after 100msecs and if the mouse is not moving it will scale the circle to 1 (default size).
+        timeout = setTimeout(function(){
+            document.getElementById("mouseChaserCircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
 
-}
+        }, 100)
 
-
-function mouseCursorChaser(xScale, yScale){
-    window.addEventListener("mousemove", function(getDetails){
-        this.document.getElementById("mouseChaserCircle").style.transform = `translate(${getDetails.clientX}px,${getDetails.clientY}px) scale(${xScale},${yScale})`;
     })
 }
-// circleCompressor();
-mouseCursorChaser();
+
+mouseChaserCircle();
+
+
+
