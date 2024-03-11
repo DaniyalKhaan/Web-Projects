1f29aebf48d455bdee9ad08a61b64332cbd93599
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-03-11 21:56:41 +0500

    working on elements

--- my_First_Website_Project/script.js
+++ my_First_Website_Project/script.js
@@ -4,6 +4,21 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+
+document.querySelectorAll(".elem").forEach(function(elem){
+    elem.addEventListener("mousemove", function(dets){
+        // let diff = dets.clientY - elem.getBoundingClientRect().top();
+        // console.log(dets)
+        gsap.to(elem.querySelector("img"), {
+            opacity: 1,
+            ease: Power1,
+        });
+    });
+});
+
+
+
+
 // Start - global variables used in mouseChaserCircle
 let xprev = 0;
 let yprev = 0;
@@ -13,11 +28,11 @@ let cirleSizeToDefault = 0;
 let timeout;
 //  End - global variables used in mouseChaserCircle
 
-function mouseChaserCircle() {
+function cursorChaserCircle() {
     clearTimeout(timeout);  // when mouse is moving value of timeout will be zero.
     window.addEventListener("mousemove", function(dets){
         xScale = gsap.utils.clamp(.8, 1.2 , dets.clientX - xprev);
-        yScale = gsap.utils.clamp(.7, 1.2 , dets.clientY - yprev);
+        yScale = gsap.utils.clamp(.8, 1.2 , dets.clientY - yprev);
 
         xprev = dets.clientX;
         yprev = dets.clientY;
@@ -33,7 +48,4 @@ function mouseChaserCircle() {
     })
 }
 
-mouseChaserCircle();
-
-
-
+cursorChaserCircle();
